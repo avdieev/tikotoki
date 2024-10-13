e48a0791163f2564f94ef07292240a065b23ba03
avdieev <37574206+avdieev@users.noreply.github.com>
2024-10-13 05:22:42 +0300

    updates scripts

--- funcs/functions.js
+++ funcs/functions.js
@@ -1,9 +1,8 @@
-
 const axios = require('axios');
 const cheerio = require('cheerio');
 
 async function getRandom(ext) {
-    return `${Math.floor(Math.random() * 10000)}${ext}`;
+    return `${Math.floor(Math.random() * 10000)}${ext}`
 }
 
 async function getBuffer(url) {
@@ -16,11 +15,11 @@ async function getBuffer(url) {
         'Upgrade-Insecure-Requests': 1
       },
       responseType: 'arraybuffer'
-    });
-    return data.data;
+    })
+    return data.data
   } catch (err) {
     console.log(err);
-    return err;
+    return err
   }
 }
 
@@ -33,25 +32,10 @@ function htmlToText(html) {
   return $.text();
 }
 
-// Функция для проверки статуса бана пользователя
-async function getBanned(chatId) {
-  const bannedUsers = [123456789, 987654321]; // Пример массива забаненных пользователей
-  if (bannedUsers.includes(chatId)) {
-    return {
-      status: false,
-      reason: "You are banned for violating the rules."
-    };
-  } else {
-    return {
-      status: true
-    };
-  }
-}
 
 module.exports = {
   getBuffer,
   htmlToText,
   filterAlphanumericWithDash,
-  getRandom,
-  getBanned // Экспортируем функцию getBanned
-};
+  getRandom
+}
